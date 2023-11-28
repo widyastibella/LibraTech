aa8304f71a6ba2bc2f52d9a325781662a34e5a36
Widyasti Bella Kurnia <112605699+widyastibella@users.noreply.github.com>
2023-11-28 22:15:56 +0700

    Admin.java

--- src/libratech/Admin.java
+++ src/libratech/Admin.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package libratech;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Fuad.H
- */
 public class Admin {
     private int idAdmin;
     private String namaAdmin;
